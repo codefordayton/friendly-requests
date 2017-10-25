1b9ce2059fa7fbc8786d590aeec8439f41d3f3dd
Matt Anderson <matt@mattbanderson.com>
2017-10-24 20:32:17 -0400

    Integrate RequestForm component into new layout.

--- friendly-requests-client/src/App.js
+++ friendly-requests-client/src/App.js
@@ -2,34 +2,6 @@ import React, {Component} from 'react';
 import './App.css';
 import FixedMenuLayout from './containers/FixedMenuLayout';
 
-import Input from 'muicss/lib/react/input';
-import Button from 'muicss/lib/react/button';
-
-class RequestForm extends Component {
-    render() {
-        return (
-            <div>
-                <form action="http://127.0.0.1:5000/generate" method="POST">
-                    <legend>Friendly Form</legend>
-                    <Input name="department_contact_name" hint="department_contact_name"/>
-                    <Input name="department_address1" hint="department_address1"/>
-                    <Input name="department_address2" hint="department_address2"/>
-                    <Input name="department_name" hint="department_name"/>
-                    <Input name="copy_files" hint="copy_files"/>
-                    <Input name="request_description" hint="request_description"/>
-                    <Input name="nte_cost" hint="nte_cost"/>
-                    <Input name="waive_fees" hint="waive_fees"/>
-                    <Input name="request_goals" hint="request_goals"/>
-                    <Input name="date_needed" hint="date_needed"/>
-                    <Input name="phone_number" hint="phone_number"/>
-                    <Input name="requester_name" hint="requester_name"/>
-                    <Button variant="raised">Submit</Button>
-                </form>
-            </div>
-        );
-    }
-}
-
 class App extends Component {
   render() {
     return (
--- friendly-requests-client/src/containers/HomePage.js
+++ friendly-requests-client/src/containers/HomePage.js
@@ -1,19 +1,11 @@
 import React from 'react'
-import { Container, Header, Image } from 'semantic-ui-react'
+import { Container } from 'semantic-ui-react';
+import RequestForm from './RequestForm';
+
 
 const HomePage = () => (
   <Container text style={{ marginTop: '7em' }}>
-    <Header as='h1'>Semantic UI React Fixed Template</Header>
-    <p>This is a basic fixed menu template using fixed size containers.</p>
-    <p>A text container is used for the main container, which is useful for single column layouts.</p>
-
-    <Image src='/assets/images/wireframe/media-paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
-    <Image src='/assets/images/wireframe/paragraph.png' style={{ marginTop: '2em' }} />
+    <RequestForm />
   </Container>
 )
 
