df5399ad355d4d3e97bcb1337aaf3f5be9d1b3d9
Matt Anderson <matt@mattbanderson.com>
2018-10-22 21:46:04 -0400

    Fix broken icon image and hide footer.

--- friendly-requests-client/src/containers/NavMenu.js
+++ friendly-requests-client/src/containers/NavMenu.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Container, Image, Menu } from 'semantic-ui-react'
+import { Container, Icon, Image, Menu } from 'semantic-ui-react'
 
 const NavMenu = () => (
   <Menu fixed='top' inverted>
     <Container>
       <Menu.Item as='a' header>
-        <Image
-          size='mini'
-          src='/logo.png'
-          style={{ marginRight: '1.5em' }}
-        />
+        <Icon name='archive' size='large' />
         Friendly Requests
       </Menu.Item>
       <Menu.Item as='a'><Link to="/">Home</Link></Menu.Item>
